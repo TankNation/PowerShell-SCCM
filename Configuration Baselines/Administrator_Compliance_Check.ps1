e2f8d4b4ea611d4b19be9ef888e4e059ac75fb71
Brian T <tanknospam@gmail.com>
2017-02-13 12:51:59 -0700

    Add files via upload

---
+++ Configuration Baselines/Administrator_Compliance_Check.ps1
@@ -0,0 +1,59 @@
+﻿#############################################################################################################################################
+###~ Administrator Compliance Check - SCCM Baseline Script ##################################################################################
+#
+#~ Brian Tancredi
+#~ Created: 2017-01-09
+#~ Modified: 2017-01-27
+#
+#~ References:
+#~ https://kareembehery.wordpress.com/2016/04/04/dcm-in-sccm-2012-members-in-local-admin-group-compliance/
+#
+#############################################################################################################################################
+
+#############################################################################################################################################
+###~ Assigning Variables and Functions ######################################################################################################
+#############################################################################################################################################
+
+$adminusers = $true
+$local_Group = "Administrators"
+$ok_Admins = @(
+"administrator",
+"Domain\Domain Admins"
+)#~ Acceptable Administrators
+$local_Admins = net localgroup $local_Group | where {$_ -AND $_ -notmatch “command completed successfully”} | select -skip 4
+#$useraffinity = gwmi -Namespace root\ccm\policy\machine -Class ccm_useraffinity
+
+function NonCompliant_No_Admin{
+    $present = 0
+    foreach ($admin in $ok_Admins){
+        If ($local_Admins -contains $admin){$present = $present + 1}}
+    If ($present -eq 0){$Script:adminusers = $false}
+}#~ Function checks the Administrator Group contains an approved member
+
+function NonCompliant_Extra_Admin{
+    $extra = 0
+    foreach ($admin in $local_Admins){
+        If (!($ok_Admins -contains $admin)){$extra = $extra + 1}}
+    If ($extra -ge 1){$Script:adminusers = $false}
+}#~ Function compares the Administrator Group to array of compliant Administrators
+
+function Compliance{
+    Write-Host $adminusers
+}#~ Function displays Workstation Compliance
+
+#############################################################################################################################################
+###~ Run Checks against approved local Administrator list and current local Administrators to determine compliance ##########################
+#############################################################################################################################################
+
+#foreach ($useraff in $useraffinity){$ok_Admins += $useraff.ConsoleUser}
+
+#~ Checking that the Administrator Group contains an approved member
+NonCompliant_No_Admin
+#~ Comparing the Administrator Group against compliant Administrators
+NonCompliant_Extra_Admin
+#~ Display workstation compliance
+Compliance
+
+#############################################################################################################################################
+#~ END ######################################################################################################################################
+#############################################################################################################################################
---
+++ Configuration Baselines/AnyConnect_AutoUpdate_Compliance_Remediation.ps1
@@ -0,0 +1,65 @@
+﻿#############################################################################################################################################
+###~ Configure Cisco AnyConnect VPN Profiles to Check for Updates on Connecting #############################################################
+#
+#~ Brian Tancredi
+#~ Created: 2017-01-25
+#~ Modified: 2017-01-25
+#
+#~ References:
+#~ http://stackoverflow.com/questions/16428559/powershell-script-to-update-xml-file-content
+#
+#############################################################################################################################################
+
+#############################################################################################################################################
+###~ Assign Variables and Declaring Functions ###############################################################################################
+#############################################################################################################################################
+
+#~ Define Variables
+$user_Path = "$env:HomeDrive\users"
+$xml_Pref = "preferences.xml"
+$non_Compliant_Text = "<AutoUpdate>false</AutoUpdate>"
+$compliant_Text = "<AutoUpdate>true</AutoUpdate>"
+$insert_Start_Location = "<ControllablePreferences>"
+$insert_End_Location = "</ControllablePreferences>"
+$xml_End_Location = "</AnyConnectPreferences>"
+
+
+function targetXML{
+    $cisco_Path = "$user_Path\$user\AppData\Local\Cisco\Cisco AnyConnect Secure Mobility Client"
+    $Script:xml_Path = "$cisco_Path\$xml_Pref"
+} #~ Assigning path variables based on $user
+
+#############################################################################################################################################
+###~ Rewrite XML Preference File to AutoUpdate AnyConnect Client ############################################################################
+#############################################################################################################################################
+
+$users = Get-ChildItem "$user_Path" -Name
+foreach ($user in $users){
+    targetXML
+    If (Test-Path $xml_Path){
+        $get_Content = Get-Content $xml_Path
+        $test_Non_Compliant = $get_Content | %{$_ -match $non_Compliant_Text}
+        $test_Compliant = $get_Content | %{$_ -match $compliant_Text}
+        $test_Start_Location = $get_Content | %{$_ -match $insert_Start_Location}
+
+        #~ Replace Non-Compliant Text in XML if Present
+        If ($test_Non_Compliant -contains $non_Compliant_Text){
+            $get_Content | % { $_.Replace($non_Compliant_Text, $compliant_Text)} | Set-Content $xml_Path
+        }
+        #~ Compliant Text in XML is Present (Do Nothing)
+        ElseIf ($test_compliant -contains $compliant_Text){}
+        #~ Place Compliant Text in XML if Not-Present
+        Else { 
+            If ($test_Start_Location -contains $insert_Start_Location){
+                $get_Content | % { $_.Replace($insert_Start_Location, "$insert_Start_Location`n$compliant_Text")} | Set-Content $xml_Path
+            }
+            Else {
+                $get_Content | % { $_.Replace($xml_End_Location, "$insert_Start_Location`n$compliant_Text`n$insert_End_Location`n$xml_End_Location")} | Set-Content $xml_Path
+            }
+        }
+    }
+}
+
+#############################################################################################################################################
+#~ END ######################################################################################################################################
+#############################################################################################################################################
